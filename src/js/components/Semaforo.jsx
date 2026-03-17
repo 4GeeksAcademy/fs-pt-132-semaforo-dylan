7d768c488c8be602bbb79f35e1942ccd8761f47d
dylanrestrepomst <dylanrestrepo.mst@gmail.com>
2026-03-17 19:47:26 +0000

    segunda actualizacion

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Semaforo = () => {
 
     const [color, setColor] = useState('');
-
+    const [contador, setContador] =useState(0);
     const handleClick =(ligth)=>{
         // si  el color de useState es igual a ligth entonces me cambias color de nuevo a string vacio para quitar el active
     if(color===ligth ){
@@ -12,7 +12,25 @@ const Semaforo = () => {
     }
         setColor(ligth)
     }
+  
+    const handleBotton=()=>{
 
+    let nuevo=(contador+1)%4;
+    setContador(nuevo);
+    if(nuevo===0){
+        return setColor("")
+    }
+    if(nuevo===1){
+        return setColor("red")
+    }
+    if(nuevo===2){
+        return setColor("yellow")
+    }
+    if(nuevo===3){
+        return setColor("green")
+    }
+    
+   }
     return (
     <div className="semaforo">
          {/* ponemos una ternaria para  añadirle una clase active si  color es igual a red sino sigue devolviendo un class vacio */}
@@ -25,10 +43,9 @@ const Semaforo = () => {
         <div className={ `circleGreen  ${color=="green" ? "active": "" }`} onClick={()=>handleClick("green")}>
 
         </div>
-
+  <button type="button" className="btn btn-primary" onClick={()=>handleBotton()}>Luz</button>
     </div>
-
-
+  
     );
 
 };
